61ca38b3d7496414659c4aaf08f8b5947ac766b0
Jon Owings <jon.owings@gmail.com>
2019-04-04 14:52:18 -0400

    d

--- jobs/psoprereqs-pkg-install/templates/bin/pre-start.sh
+++ jobs/psoprereqs-pkg-install/templates/bin/pre-start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 set -x
-apt-get update
-apt-get -f install -y
-apt-get install -y multipath-tools open-iscsi
 
-#cd /var/vcap/packages/psoprereqs/apt/cache/archives
-#chmod +x install-packages.sh
-#./install-packages.sh
+cd /var/vcap/packages/psoprereqs/apt/cache/archives
+chmod +x install-packages.sh
+. install-packages.sh
